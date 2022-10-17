f77649577a5a446996018f19cd4e9e1176e0a7d5
eyupGuc <meydanguresi@gmail.com>
2022-10-17 09:08:40 +0300

    add

--- src/card/CardItem.jsx
+++ src/card/CardItem.jsx
@@ -1,10 +1,15 @@
 import Container from 'react-bootstrap/Container';
-import  Image  from "react-bootstrap/Image"
+import  Image  from "react-bootstrap/Image";
 
-const CardItem=()=>{
+
+const CardItem=(props)=>{
+    console.log(props);
+    const{id,img,info}=props;
+console.log(props[1])
+
     return (
         <Container className='CardItem'>
-<Image />
+<h2>{props[1].id}</h2>
         </Container>
     )
 }
---
+++ src/data.js
@@ -0,0 +1,26 @@
+const data=[
+    {
+        id:1,
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Spanish_Gypsy_Girls_NGM-v31-p267.jpg/800px-Spanish_Gypsy_Girls_NGM-v31-p267.jpg",
+        info:"İspanyol Romanları, National Geographic Magazine, Sayı 31 (1917), sayfa 267."
+
+    },{
+        id:2,
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/T%C3%BCrkiye_%C3%87ingeneleri_ve_Oranlar%C4%B1_2010.png/800px-T%C3%BCrkiye_%C3%87ingeneleri_ve_Oranlar%C4%B1_2010.png",
+        info:"Türkiye Çingeneleri ve Oranları 2010"
+
+    },{
+        id:3,
+        img:"https://www.dokuzeylul.com/images/upload/cYingene12.jpg",
+        info:"Eski göçebe yaşamlarına uygun işler yaparlardı. Kadınlar falcılık yapar, bohçacılık yapar, dilenir ya da dans ederdi. Erkekler ise çalgı çalar, kap kacak lehimciliği, kalaycılık, hayvan ticareti, hayvan eğiticiliği gibi işlerle uğraşırlardı. Geçmişte atlarla çekilen arabalarla yapılan göçlerde artık kamyon ya da karavanlar kullanılmaktadır. Eski uğraşlarının yerini ise, meyve toplama, asfalt dökme, kullanılmış araba ticareti, sirklerde hayvan bakıcılığı ya da eğiticiliği, hurda maden ve antika eşya alım satımı gibi işler almış olmakla birlikte özellikle Türkiye’de ve diğer birçok dünya ülkesinde yerleşik hayata geçmiş olanlar diğer meslek dallarıyla ilgilenmiş olup başka türlü meslekler de edinmişlerdir."
+
+    },{
+        id:4,
+        img:"https://cdn1.ntv.com.tr/gorsel/bRgNdm4qp0mdsggj7ei_lg.jpg?width=991&mode=both&scale=both&v=1622190763852",
+        info:"Cingen-Muğla,  Cono-Adana, Cındar-Adıyaman,Roman-İzmir,Cingan(Bolu, Kastamonu, Çankırı, Sinop, Çorum, Afyon, Ankara, Kırşehir, Kırıkkale, Kayseri, Yozgat),Gurbet(Tunceli, KKTC, Hatay, Gaziantep)"
+        
+
+    },
+];
+
+export default data;
